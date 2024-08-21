fb67999fcb8adc9c7ea906704417cc9b1956179e
Ahmet Kale <116072726+ahmetkale35@users.noreply.github.com>
2024-08-22 00:36:43 +0300

    Add files via upload

---
+++ Week_09_Lab/src/Rectangle.java
@@ -0,0 +1,11 @@
+
+public class Rectangle extends Shape2D{
+
+	public Rectangle(double height, double width) {
+		super(height, width);
+	}
+	public double getArea(int height, int width) {
+		double area;
+		return area = height * width;
+	}	
+}
---
+++ Week_09_Lab/src/Square.java
@@ -0,0 +1,13 @@
+
+public class Square extends Shape2D{
+
+	public Square(double width) {
+		super(width);
+	}
+	
+	public double getArea(double width) {
+		double area;
+		return area = width * width;
+	}
+	
+}
