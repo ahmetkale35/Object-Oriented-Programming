f928cdb7a291bb91ddc6036b977f0f32b24230cf
Ahmet Kale <116072726+ahmetkale35@users.noreply.github.com>
2024-08-23 23:35:01 +0300

    Delete Week_10_Lab directory

--- Week_10_Lab/Week10_Lab1/src/ConferenceScoreBehaviour.java
+++
@@ -1,4 +0,0 @@
-
-public interface ConferenceScoreBehaviour {
-    float conferenceScore();
-}
--- Week_10_Lab/Week10_Lab2/src/Eagle.java
+++
@@ -1,9 +0,0 @@
-class Eagle extends Animal {
-    public void fly() {
-        System.out.println("Eagle is flying.");
-    }
-
-    public void move() {
-        fly();
-    }
-}
